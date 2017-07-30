6c5d86fee0311f2a271d75db3cee17c2c68ee116
Kong Lingbo <konglingbowo@163.com>
2017-07-30 20:58:53 +0800

    Update get fitment.py

--- get fitment.py
+++ get fitment.py
@@ -10,21 +10,14 @@ for i in range(0, 675, 25):
     page_url = requests.get(url, headers=head)
     page_get = page_url.text
     page_text = BeautifulSoup(page_get, 'lxml')
-    fitment_1 = page_text.find_all('tr', {'class': 'fitment listRowEven'})
-    fitment_2 = page_text.find_all('tr', {'class': 'fitment listRowOdd'})
-    for each_tag_1 in fitment_1:
-        td_text_1 = each_tag_1.find_all('td')
+    fitment = page_text.find_all('tr', {'class': re.compile('fitment')})
+    
+    for each in fitment:
+    td = each.find_all('td')
         for x in range(10):
-            fx = open('fitment/{}.txt'.format(x), 'a')
+            f = open('fitment/{}.txt'.format(x), 'a')
             try:
-                print(td_text_1[x].string, file=fx)
+                print(td[x].string, file=f)
             except IndexError:
                 print(None, file=fx)
-    for each_tag_2 in fitment_2:
-        td_text_2 = each_tag_2.find_all('td')
-        for y in range(10):
-            fy = open('fitment/{}.txt'.format(y), 'a')
-            try:
-                print(td_text_2[y].string, file=fy)
-            except IndexError:
-                print(None, file=fy)
+   
